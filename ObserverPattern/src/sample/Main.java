474f1dedd1c229e3ec7e6f06ab0494ab1f78ea07
NiklasHj <niklas.hjelm@ga.ntig.se>
2020-01-31 12:21:15 +0100

    hej

---
+++ ObserverPattern/src/sample/Main.java
@@ -0,0 +1,17 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
